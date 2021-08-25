3f86d31390844e1b9e7b4e7f4f2a51db5faa3d45
Markus Peitl <office@markuspeitl.com>
2021-08-25 12:04:53 +0200

    Initial commit

---
+++ src/functional-tool.ts
@@ -0,0 +1,47 @@
+export class FunctionalTool {
+	public static MultiplyFn<ItemType>(singleFn: (item: ItemType) => any, array: ItemType[]): void {
+		for (const element of array) {
+			singleFn(element);
+		}
+	}
+	public static MultiplyFnCollect<ItemType, ReturnType>(singleFn: (item: ItemType) => ReturnType, array: ItemType[]): ReturnType[] {
+		const results: ReturnType[] = [];
+		for (const element of array) {
+			results.push(singleFn(element));
+		}
+		return results;
+	}
+	public static walkPropTree(rootNode: any, leafFn: (leaf: any) => any, nodeFn: (node: any) => any, arrayFn: (array: any) => any): void {
+		for (const key in rootNode) {
+			if (Object.prototype.hasOwnProperty.call(rootNode, key)) {
+				const selectedProp: any = rootNode[key];
+				if (Array.isArray(selectedProp)) {
+					arrayFn(selectedProp);
+					for (const element of selectedProp) {
+						FunctionalTool.walkPropTree(element, leafFn, nodeFn, arrayFn);
+					}
+				} else if (typeof selectedProp === 'object') {
+					nodeFn(selectedProp);
+					FunctionalTool.walkPropTree(selectedProp, leafFn, nodeFn, arrayFn);
+				} else {
+					leafFn(selectedProp);
+				}
+			}
+		}
+	}
+
+	public static async ArrayCallPromiseCollect(targetObjects: any[], targetMethodMember: string, args: any[]): Promise<any[]> {
+		const promises: Promise<any>[] = [];
+		for (const target of targetObjects) {
+			promises.push(target[targetMethodMember](...args));
+		}
+		return await Promise.all(promises);
+	}
+
+	public static removeArrayItem<ItemType>(array: ItemType[], item: ItemType): void {
+		const activeSubIndex: number = array.indexOf(item);
+		if (activeSubIndex > -1) {
+			array.splice(activeSubIndex);
+		}
+	}
+}
