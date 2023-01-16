5ea2dc7f3bc92c94b42dad740034e9a11eaebc0b
Markus Peitl <office@markuspeitl.com>
2023-01-16 18:49:05 +0100

    - Added function to get only unique items of array - Added abstraction function for tree transversal - Added implmentations of tree transversal for directory processing and even more concrete recursive copying

--- src/functional-tool.ts
+++ src/functional-tool.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+import * as syspath from 'path';
 export class FunctionalTool {
 	public static multiplyFn<ItemType>(singleFn: (item: ItemType) => any, array: ItemType[]): void {
 		for (const element of array) {
@@ -29,7 +31,6 @@ export class FunctionalTool {
 			}
 		}
 	}
-
 	public static arrayCallPromiseCollect(targetObjects: any[], targetMethodMember: string, args: any[]): Promise<any[]> {
 		const promises: Promise<any>[] = [];
 		for (const target of targetObjects) {
@@ -44,4 +45,56 @@ export class FunctionalTool {
 			array.splice(activeSubIndex);
 		}
 	}
+
+	public static unique<Type>(array: Type[]): Type[] {
+		return array.filter((item: Type, index: number, toFilterArray: Type[]) => toFilterArray.indexOf(item) === index);
+	}
+
+	public static walkTree<Type>(rootNode: Type, walkOptions: IWalkOptions<Type>): void {
+		const children: any[] = walkOptions.getChildren(rootNode);
+
+		for (const child of children) {
+			if (walkOptions.shouldTransverse(child)) {
+				FunctionalTool.walkTree(child, walkOptions);
+			}
+			walkOptions.processChild(child);
+		}
+	}
+
+	public static walkDirectoryRecursive(path: string, walkDirOptions: IWalkDirOptions): void {
+		const walkOptions: IWalkOptions<string> = {
+			getChildren: (path: string) => {
+				const relChildPaths: string[] = fs.readdirSync(path);
+				return relChildPaths.map((relpath: string) => syspath.join(path, relpath));
+			},
+			shouldTransverse: (path: string) => {
+				walkDirOptions.processDirectory(path);
+				return fs.lstatSync(path).isDirectory();
+			},
+			processChild: walkDirOptions.processFile
+		};
+		return FunctionalTool.walkTree<string>(path, walkOptions);
+	}
+
+	public copyDirectoryRecursive(sourcePath: string, targetPath: string) {
+		const walkOptions: IWalkDirOptions = {
+			processFile: (path: string) => {
+				const fileName: string = syspath.basename(path);
+				const targetFilePath: string = syspath.join(targetPath, fileName);
+				fs.copyFileSync(path, targetFilePath);
+			},
+			processDirectory: (path: string) => {}
+		};
+		return FunctionalTool.walkDirectoryRecursive(sourcePath, walkOptions);
+	}
+}
+export interface IWalkOptions<Type> {
+	getChildren(node: Type): any[];
+	shouldTransverse(child: any): boolean;
+	processChild(child: any): void;
+}
+
+export interface IWalkDirOptions {
+	processFile(filePath: string): void;
+	processDirectory(directoryPath: string): void;
 }
